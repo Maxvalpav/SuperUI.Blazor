619452ac1df4e4d94ba4b868df1ea2771eb3144c
maxval88 <maxvaltel@gmail.com>
2026-05-09 00:48:18 +0500

    fix spinner

---
+++ SuperUI/Components/SgSpinnerSpeed.cs
@@ -0,0 +1,16 @@
+namespace SuperUI.Components;
+
+/// <summary>
+/// Defines the animation speed of the <see cref="SgSpinner"/> component.
+/// </summary>
+public enum SgSpinnerSpeed
+{
+    /// <summary>Normal animation speed (default).</summary>
+    Normal,
+
+    /// <summary>Slow animation speed.</summary>
+    Slow,
+
+    /// <summary>Fast animation speed.</summary>
+    Fast
+}
