0d5f86ac3ad35a8c90dadb0ab7924beef92ee06d
maxval88 <maxvaltel@gmail.com>
2026-05-09 00:02:43 +0500

    fix spinner

---
+++ SuperUI/Components/SgSpinnerVariant.cs
@@ -0,0 +1,19 @@
+namespace SuperUI.Components;
+
+public enum SgSpinnerVariant
+{
+    /// <summary>Default (accent color).</summary>
+    Default,
+
+    /// <summary>Info-like accent.</summary>
+    Info,
+
+    /// <summary>Success-like accent.</summary>
+    Success,
+
+    /// <summary>Warn-like accent.</summary>
+    Warn,
+
+    /// <summary>Danger-like accent.</summary>
+    Danger
+}
